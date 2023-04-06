0c4e73f8da4cd233cd208acb181ae863f0b66bc2
Bruno Carvalho <bruno.js.carvalho123@gmail.com>
2023-04-06 17:40:03 +0100

    Fix last commit

--- casper-tooltip.js
+++ casper-tooltip.js
@@ -212,7 +212,7 @@ class CasperTooltip extends PolymerElement {
     const purifiedHTML = DOMPurify.sanitize(tooltipText, {
       CUSTOM_ELEMENT_HANDLING: {
           tagNameCheck: /^casper-|^day-detail-/, // only casper elements are allowed
-          attributeNameCheck: /icon|tooltipObject/, // only icon attribute is allowed
+          attributeNameCheck: /tooltipObject/, // only icon attribute is allowed
           allowCustomizedBuiltInElements: false, // no customized built-ins allowed
       },
     });
